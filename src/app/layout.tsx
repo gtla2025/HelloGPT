309d7bb7ee6748e8a2ec03ab593aedbccaf4d9bd
gtla2025 <41835001+gtla2025@users.noreply.github.com>
2024-05-28 21:48:09 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,11 +31,12 @@ const RootLayout = async ({ children, modal }: RootLayoutProps) => {
           <AuthProvider>
             <div className="HelloGPT">
               <h1><img src="https://dl.cmdpe.com/HelloGPT/HelloGPT-Logo.png"/></h1>
+        		  <div></div>
               <ul>
-                <li><a href="https://pay.cmdpe.link">获取KEY</a></li>
-                <li><a href="https://pay.cmdpe.link">获取密码</a></li>
-                <li><a href="https://www.cmdpe.com/213.html">学习群</a></li>
-                <li><a href="https://www.cmdpe.com/category-10.html">软件下载</a></li>
+                <li><a href="https://pay.cmdpe.link" target="_blank">获取KEY</a></li>
+                <li><a href="https://pay.cmdpe.link" target="_blank">获取密码</a></li>
+                <li><a href="https://www.cmdpe.com/213.html" target="_blank">学习群</a></li>
+                <li><a href="https://www.cmdpe.com/category-10.html" target="_blank">软件下载</a></li>
               </ul>	  
             </div>
             {children}
