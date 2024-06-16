62355e1f0ab9702f2c00378b30001995115bb6c9
gtla2025 <41835001+gtla2025@users.noreply.github.com>
2024-06-16 16:08:59 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,6 @@ const RootLayout = async ({ children, modal }: RootLayoutProps) => {
       <body>
         <GlobalProvider>
           <AuthProvider>
-            <div className="HelloGPT">
-            	<h1><img src="https://raw.githubusercontent.com/gtla2025/SSR/master/HelloGPT-Logo.png"/></h1>  
-            </div>
             {children}
             {modal}
           </AuthProvider>
